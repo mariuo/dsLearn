8a848ccdf424853e8b48e3a0e13eb077ecdefefc
mariuo <mariuo@gmail.com>
2021-12-08 09:19:30 -0500

    Adding Query JPQL Notifications

--- backend/src/main/java/com/devsuperior/dslearnbds/resources/NotificationResource.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/resources/NotificationResource.java
@@ -5,8 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dslearnbds.dto.NotificationDTO;
@@ -20,8 +20,11 @@ public class NotificationResource {
 	private NotificationService service;
 
 	@GetMapping
-	public ResponseEntity<Page<NotificationDTO>> findByI(Pageable pageable){
-		Page<NotificationDTO> page = service.notificationForCurrentUser(pageable);
+	public ResponseEntity<Page<NotificationDTO>> notificationsForCurrentUser(
+			@RequestParam(name = "unreadOnly", defaultValue="false")Boolean unreadOnly,
+			Pageable pageable){
+		
+		Page<NotificationDTO> page = service.notificationForCurrentUser(unreadOnly, pageable);
 		return ResponseEntity.ok().body(page);
 	}
 }
